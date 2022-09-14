d14a643abb479dc3b4125aec9c0ae0fdd7811c60
aluno <rodrigolpj@hotmail.com>
2022-09-14 10:40:39 -0300

    Commit 10

--- MovieRentalApp/Rental.java
+++ MovieRentalApp/Rental.java
@@ -20,13 +20,9 @@ public class Rental {
       return _movie.getCharge(_daysRented);
 
    }
+   
+   
    public int getFrequentRenterPoints(){
-   	int frequentRenterPoints = 0;
-   	// add frequent renter points
-        frequentRenterPoints ++;
-        // add bonus for a two day new release rental
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            getDaysRented() > 1) frequentRenterPoints ++;
-   	return frequentRenterPoints;
+      return _movie.getFrequentRenterPoints(_daysRented);
    }
 }
